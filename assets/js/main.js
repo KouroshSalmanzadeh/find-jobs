f79fdea7e382dbd2a08e4e2ea77810da233071bf
KouroshSalmanzadeh <kouroshsalmanzadeh@gmail.com>
2023-12-06 19:48:19 +0330

    Add showing box when scrolling and when loading page #27

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,4 +18,37 @@ function ShowPassword() {
     y.className = "fa-solid fa-eye-slash fa-lg blue-color m-r-10 hov-pointer";
   }
 }
-// for show password of Login page or sign up page ____Start____
+// for show password of Login page or sign up page ____End____
+
+
+// Show box when scrolling  ____Start____
+function reveal() {
+  const reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 70;
+
+    if (elementTop < innerHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    }
+    else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
+// Show box when scrolling  ____End____
+
+// show box when page is loading ___Start___
+function loadPage() {
+  const hidebox = document.querySelectorAll(".hide-box");
+
+  for (var i = 0; i < hidebox.length; i++) {
+    hidebox[i].classList.add("active-box");
+
+  }
+}
+// show box when page is loading ___End___
